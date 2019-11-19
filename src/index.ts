c3c39540cfad06212042894ea98cd9d74fc51d5d
John Blackwell <jblackwell072588@gmail.com>
2019-11-19 11:23:43 -0800

    write functions

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,35 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+import { scaleLinear } from 'd3-scale';
+
+export const generateGradientSteps = (
+  colors: string[] = ['#000', '#fff'],
+  steps: number = 10
+) => {
+  const colorScale = scaleLinear<string>()
+    .domain([0, 1])
+    .range(colors);
+  const gradientSteps = colorScale.ticks(steps).map(value => colorScale(value));
+
+  return gradientSteps;
+};
+
+interface GradientCSSStringOptions {
+  customStepDirection?: string;
+  customStepStops?: Array<string | undefined>;
+}
+
+export const generateGradientCSSString = (
+  colors: string[] = ['#000', '#fff'],
+  steps: number = 10,
+  options: GradientCSSStringOptions = {}
+) => {
+  const { customStepDirection = '45deg', customStepStops = [] } = options;
+  const gradientSteps = generateGradientSteps(colors, steps);
+  if (customStepStops.length > 1) {
+    const transformedSteps = gradientSteps.map(
+      (step, i) => `${step}${i > 0 && customStepStops[i] !== undefined ? ` ${customStepStops[i]}` : ''}`);
+    const gradientCSSString = `linear-gradient(${customStepDirection}, ${transformedSteps.join(', ')});`;
+    return gradientCSSString;
   }
-  return a + b;
+  const gradientCSSString = `linear-gradient(${customStepDirection}, ${gradientSteps.join(', ')});`;
+  return gradientCSSString;
 };
